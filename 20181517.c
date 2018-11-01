d778eee7bf26d58aa5e6ba8e8e3b565dd5bced0e
splo2t <yu990410@naver.com>
2018-11-01 15:40:56 +0900

    rfibo

---
+++ 20181517.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long rfibo(int n);
+long sfibo(int n);
+//1 1 2 3 5
+int main() {
+    int n;
+    for(n = 3; n <= 45; n++){
+        if(n % 2 == 1){
+            printf("rfibo(%d) = %ld\n", n, rfibo(n));
+           // printf("sfibo(%d) = %ld\n", n, sfibo(n));
+        }
+    }
+    return 0;
+}
+
+long rfibo(int n){
+    if (n == 1) return 1;
+    if (n == 2) return 1;
+
+    return rfibo(n-1) + rfibo(n-2);
+}
+
